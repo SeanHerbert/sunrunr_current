282a36ce055251ccaa6eb3882730ecb7fc3da50d
EC2 Default User <ec2-user@ip-172-31-26-255.us-east-2.compute.internal>
2019-11-22 05:36:45 +0000

    added strong password requirements to register.js

--- public/fitnessPal/javascripts/register.js
+++ public/fitnessPal/javascripts/register.js
@@ -6,9 +6,13 @@ function sendRegisterRequest() {
   
   // Check to make sure the passwords match
   // FIXME: Check to ensure strong password 
-  if (password != passwordConfirm) {
-    $('#ServerResponse').html("<span class='red-text text-darken-2'>Passwords do not match.</span>");
-    $('#ServerResponse').show();
+  // if (password != passwordConfirm) {
+  //   $('#ServerResponse').html("<span class='red-text text-darken-2'>Passwords do not match.</span>");
+  //   $('#ServerResponse').show();
+  //   return;
+  // }
+
+  if(regNotValid()){
     return;
   }
   
@@ -22,6 +26,129 @@ function sendRegisterRequest() {
     .done(registerSuccess)
     .fail(registerError);
 }
+
+function regNotValid(){
+  elist= "<div class='red-text text-darken-2'><ul>";
+  document.getElementById("ServerResponse").innerHTML="";
+  nameRed();
+  mailRed();
+
+  passRed();
+  elist+="</div></ul>";
+  if(elist !== "<ul></ul>"){
+    console.log(elist);
+    document.getElementById("ServerResponse").innerHTML=elist;
+    return true;
+  }
+  else{
+    return false;
+  }
+
+  
+
+
+
+}
+
+function mailRed(){
+  var reg =/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,5}$/;
+  var red = $('#email');
+  if(!reg.test(red.val())){
+    // red.classList.add("error");
+     document.getElementById("ServerResponse").style.display="block";
+
+     elist += "<li>Invalid or missing email address.</li>";
+  }
+  else{
+    document.getElementById('email').removeAttribute("class");
+    document.getElementById("ServerResponse").style.display="none";
+
+  }
+}
+
+function passRed(){
+  var reg =/(.*[a-z].*)/;
+  var reg1 =/(.*[A-Z].*)/;
+  var reg2=/(.*\d.*)/;
+  var flag = false;
+
+
+  var red = document.getElementById('password');
+  if (document.getElementById('password').value.length<10
+ ||document.getElementById('password').value.length>20){
+   flag = true;
+    // red.classList.add("error");
+    document.getElementById("ServerResponse").style.display="block";
+
+    elist += "<li>Password must be between 10 and 20 characters.</li>";
+
+ }
+
+ if(!reg.test(red.value)){
+    flag = true;
+
+    // red.classList.add("error");
+    document.getElementById("ServerResponse").style.display="block";
+
+    elist += "<li>Password must contain at least one lowercase character.</li>";
+
+ }
+  if(!reg1.test(red.value)){
+    flag = true;
+
+    // red.classList.add("error");
+    document.getElementById("ServerResponse").style.display="block";
+
+    elist += "<li>Password must contain at least one uppercase character.</li>";
+
+ }
+  if(!reg2.test(red.value)){
+    flag = true;
+
+   // red.classList.add("error");
+   document.getElementById("ServerResponse").style.display="block";
+
+   elist += "<li>Password must contain at least one digit.</li>";
+
+ }
+  if(document.getElementById('password').value!==document.getElementById('passwordConfirm').value ){
+    flag = true;
+
+    // red.classList.add("error");
+    document.getElementById("ServerResponse").style.display="block";
+
+    elist += "<li>Password and confirmation password don't match.</li>";
+   }
+ if(flag ==false){
+   // document.getElementById('password').removeAttribute("class");
+   document.getElementById("ServerResponse").style.display="none";
+
+ }
+
+
+}
+
+
+function nameRed(){
+  var red = document.getElementById('fullName');
+
+  if(document.getElementById('fullName').value.length===0||!document.getElementById('fullName').value.replace(/\s/g, '').length){
+    // red.classList.add("error");
+    document.getElementById("ServerResponse").style.display="block";
+
+    elist += "<li>Missing full name.</li>";
+
+  }
+  else{
+    // document.getElementById('fullName').removeAttribute("class");
+    document.getElementById("ServerResponse").style.display="none";
+
+  }
+
+}
+
+
+
 
 function registerSuccess(data, textStatus, jqXHR) {
   if (data.success) {  
